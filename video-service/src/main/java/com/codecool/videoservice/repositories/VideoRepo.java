722b005c218bd7236db23d7476c7987d4f3dbc30
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-07-04 19:39:51 +0300

    Made VideoRepo extend JpaRepository
    We should have paging and sorting of collections for REST and
    JpaRepos do just that.

--- video-service/src/main/java/com/codecool/videoservice/repositories/VideoRepo.java
+++ video-service/src/main/java/com/codecool/videoservice/repositories/VideoRepo.java
@@ -1,10 +1,10 @@
 package com.codecool.videoservice.repositories;
 
 import com.codecool.videoservice.model.Video;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VideoRepo extends CrudRepository<Video, Long> {
+public interface VideoRepo extends JpaRepository<Video, Long> {
 
 }
